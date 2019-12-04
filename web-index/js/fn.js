8f607c471b5d5fa0c1dabd3f1f82e10f3ee42413
tianye1 <tianye1@360.cn>
2019-12-04 16:25:36 +0800

    add

--- web-index/js/fn.js
+++ web-index/js/fn.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 fnResize();
 window.addEventListener("resize", function() {
     fnResize()
@@ -7,14 +7,4 @@ function fnResize(){
     var docWidth = document.documentElement.clientWidth,
             body = document.getElementsByTagName('html')[0];
     body.style.fontSize = docWidth / 30 + 'px';
-=======
-fnResize();
-window.addEventListener("resize", function() {
-    fnResize()
-}, false);
-function fnResize(){
-    var docWidth = document.documentElement.clientWidth,
-            body = document.getElementsByTagName('html')[0];
-    body.style.fontSize = docWidth / 30 + 'px';
->>>>>>> f410ee65735f169ea7d2dea46971cab9201b62de
 }
